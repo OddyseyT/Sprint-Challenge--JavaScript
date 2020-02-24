a11c60b011c819c2da61f30ea49a057361bbb982
Lambda_School_Loaner_282 <lambda_school_loaner_282@Lambda-School-Loaner-282s-MacBook-Pro.local>
2020-02-23 23:38:20 -0600

    Third push. Got some more things to console properly.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -157,7 +157,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = zooAnimals.filter(animals => animals.population < 5 );
+const lowPopulationAnimals = zooAnimals.filter(animals => animals.population < 5);
 
 console.log(lowPopulationAnimals);
 
